f05ed35006fafc015c244e70c7d60f62239c6323
Serg <serg@mail.ru>
2024-06-18 20:52:48 +0300

    get/set

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ class Cat:
         self.__name = name
         self.__age = age
 
+    def set_age(self, new_age):
+        if new_age > 20 or new_age < 0 or not isinstance(new_age, int):
+            raise Exception()
+        self.__age = new_age
+
+    def get_age(self):
+        return self.__age
+
     def sleep(self, time_sleep: int):
         print(f"Котик {self.__name} спит {time_sleep}s")
         sleep(time_sleep)
@@ -28,14 +36,38 @@ class Stack:
     def __init__(self):
         self.__stack: list = []
 
-    def __str__(self):
-        return str(self.__stack)
+    def __str__(self) -> str:
+        result = '-' * 10 + "\n"
+        for item in self.__stack[::-1]:
+            result += str(item) + "\n"
+        result += "-" * 10 + "\n\n"
+        return result
 
     def __len__(self):
-        return 5
+        return len(self.__stack)
 
     def push(self, data: Union[int, str]):
         self.__stack.append(data)
 
     def pop(self):
         return self.__stack.pop()
+
+    def first_item(self):
+        # x = self.pop()
+        # self.push(x)
+        # return x
+        return self.__stack[-1]
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+print(stack)
+print(stack.pop())
+print(stack.pop())
+print(stack)
+print(stack.first_item())
+stack.push(999)
+print(stack.first_item())
